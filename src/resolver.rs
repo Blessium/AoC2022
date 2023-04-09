cd70270690a136ec74e836a86e4aaf446b68c9f8
Blessium <alessandro.melgar04@gmail.com>
2023-04-09 21:24:51 +0200

    Completed day1

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,13 +1,12 @@
 use std::fs;
 
-pub struct Resolver<T> 
-{
-   day: T 
+pub struct Resolver<T> {
+    day: T,
 }
 
-impl<T> Resolver<T> 
+impl<T> Resolver<T>
 where
-    T: Day
+    T: Day,
 {
     pub fn new(day: T) -> Self {
         Resolver { day }
@@ -19,11 +18,11 @@ where
     }
 }
 
-pub trait Day{
+pub trait Day {
     fn get_input(&self, file_name: &str) -> String {
         let mut input_file = String::from("../inputs/");
         input_file.push_str(file_name);
-        fs::read_to_string(input_file).unwrap()
+        fs::read_to_string(input_file).expect("No correct input file")
     }
-    fn solve(&self) -> &str;
+    fn solve(&self) -> String;
 }
