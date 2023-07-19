cd40f0ebdb8852e81b58eb4d972232234e8abfd8
Blessium <alessandro.melgar04@gmail.com>
2023-07-19 19:46:56 +0200

    Added template for golang aoc

---
+++ go_aoc/go.mod
@@ -0,0 +1,3 @@
+module github.com/blessium/AoC2022
+
+go 1.20
---
+++ go_aoc/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/blessium/AoC2022/days"
+	"io/ioutil"
+)
+
+func main() {
+	day := days.Day13{}
+	f := "./inputs/" + day.GetFilename()
+
+	file_name := f + "_1.txt"
+	content, err := getFileContent(file_name)
+	if err != nil {
+		panic(err.Error())
+	}
+	sol := day.Solution1(content)
+	fmt.Printf("Solution 1: %s\n", sol)
+
+	next := f + "_2.txt"
+	elements := []string{file_name, next}
+	for _, file := range elements {
+		content, err = getFileContent(file)
+		if err == nil {
+			sol = day.Solution2(content)
+			fmt.Printf("Solution 2: %s\n", sol)
+            return
+		}
+	}
+}
+
+func getFileContent(f string) (string, error) {
+	content, err := ioutil.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
